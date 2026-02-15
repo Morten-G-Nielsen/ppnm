2b87ff8b0c6d03b1e14f69e5a0bbfe3c87ff87e2
Morten-G-Nielsen <Mortengwn@live.dk>
2026-02-15 20:48:12 +0100

    templatet vec

--- exercises/vec/main.cc
+++ exercises/vec/main.cc
@@ -3,9 +3,9 @@
 #include "vec.h"
 
 int main(){
-    vec test;
-    vec a {1,2,3};
-    vec b {4,5,6};
+    vec<int> test;
+    vec<float> a {1,2,3};
+    vec<float> b {4,5,6};
 
     test.print("test = ");
     a.print("a = ");
@@ -22,11 +22,14 @@ int main(){
     (-test).print("-test = ");
     test = a - b;
     test.print("test=b-a= ");
+    int q = 2;
+    std::cout << q*test << "\n";
     std::cout << "\nnorm(test) = " << norm(test) << "\n";
     std::cout << "dot(test,a) = "<< dot(test,a) << "\n";
     std::cout << "cross(test,a) = "<< cross(test,a) << "\n";
     std::cout << "dot(test, cross(test,a)) = " << dot(test, cross(test,a)) << ", dot(a,cross(test,a)) = "<< dot(a, cross(test,a)) << "\n\n";
-    auto basis = GramSchmidt({cross(test,a),a,b});
+    std::vector<vec<float>> set = {cross(test, a), a, b};
+    auto basis = GramSchmidt(set);
     std::cout << "basis from GramSchmidt({cross(test,a),a,b})\n";
     for (auto v : basis) std::cout << v << "norm = " << norm(v) <<"\n";
     std::cout << "\ndot(basis[0], basis[1]) = " << dot(basis[0], basis[1]) << "\n";
@@ -34,5 +37,14 @@ int main(){
     std::cout << "dot(basis[1], basis[2]) = " << dot(basis[1], basis[2]) << "\n";
     std::cout << "\napprox(a, a*(1+1e-7)) = " << (approx(a, a*(1+1e-7)) ? "true":"false") << "\n";
     std::cout << "approx(a, a*(2)) = " << (approx(a, a*(2)) ? "true":"false") << "\n";
+    vec<std::complex<double>> v1({1.0, 0}, {0, 1.0}, {0, 0});
+    vec<std::complex<double>> v2({0, 1.0}, {1.0, 0}, {0, 0});
+    std::cout << "\nv1 = " << v1 << "\n";
+    v2.print("v2 = ");
+    auto d = dot(v1, v1);
+    std::cout << "Dot(v1,v1) = " << d << "\n";
+    std::cout << "Norm(v1) = " << norm(v1) << "\n";
+    std::cout << "Dot(v1,v2) = " << dot(v1,v2) << "\n";
+    std::cout << "cross(v1,v2) = "<< cross(v1,v2) << "\n";
     return 0;
 }
--- exercises/vec/vec.h
+++ exercises/vec/vec.h
@@ -1,42 +1,159 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
+#include <complex>
+
 #pragma once
+template<typename T>
 struct vec{
-  double x,y,z;
+  T x,y,z;
 
   //constructors
-  vec(double x, double y, double z) : x(x), y(y), z(z) {}
-  vec() : vec(0, 0, 0) {}
+  vec(T x, T y, T z) : x(x), y(y), z(z) {};
+  vec() : vec(0, 0, 0) {};
   vec(const vec&) = default;
   vec(vec&&) = default;
   ~vec() = default;
 
-  //assigment
+  //assigments 
   vec& operator=(const vec&) = default;
   vec& operator=(vec&&) = default;
-  
-  // arithmetic
-  vec& operator+=(const vec&);
-  vec& operator-=(const vec&);
-  vec& operator*=(double);
-  vec& operator/=(double);
 
-  void print(const std::string& s="") const;
-  friend std::ostream& operator<<(std::ostream&, const vec&);
+  template <typename U>
+    vec(const vec<U>& other) 
+        : x(static_cast<T>(other.x)), 
+          y(static_cast<T>(other.y)), 
+          z(static_cast<T>(other.z)) {}
+
+  template <typename U>
+    vec& operator=(const vec<U>& other) {
+        x = static_cast<T>(other.x);
+        y = static_cast<T>(other.y);
+        z = static_cast<T>(other.z);
+        return *this;
+    }
+
+  // arithmetic
+  template <typename U>
+  vec& operator+=(const vec<U>& v){
+    x += v.x;
+    y += v.y;
+    z += v.z;
+    return *this;
+  }
+  template <typename U>
+  vec& operator-=(const vec<U>& v){
+    x -= v.x;
+    y -= v.y;
+    z -= v.z;
+    return *this;
+  }
+
+  template <typename S>
+  vec& operator*=(S s){
+    x*=s; y*=s; z*=s;
+    return *this;
+  }
+  template <typename S>
+  vec& operator/=(S s){
+    x/=s; y/=s; z/=s;
+    return *this;
+  }
+
+  void print(const std::string& s="") const {
+    std::cout << s << x << " " << y << " " << z << std::endl;
+  }
+  friend std::ostream& operator<<(std::ostream& os, const vec& v){
+    return os << "{ " << v.x << ", " << v.y << ", " << v.z << " }";
+  }
 };
 // non-member
-vec operator-(const vec&);
-vec operator-(const vec&, const vec&);
-vec operator+(const vec&, const vec&);
-vec operator*(const vec&, double);
-vec operator*(double, const vec&);
-vec operator/(const vec&, double);
+template <typename T>
+vec<T> operator-(const vec<T>& v){
+  return vec(-v.x, -v.y, -v.z);
+}
 
-double dot(const vec&, const vec&);
-double norm(const vec&);
-vec cross(const vec&, const vec&);
+template <typename T, typename U>
+auto operator-(vec<T> v, const vec<U>& u) -> vec<decltype(v.x-u.x)>{
+  v -= u;
+  return v;
+}
 
-std::vector<vec> GramSchmidt(const std::vector<vec>& input);
+template <typename T, typename U>
+auto operator+(vec<T> v, const vec<U>& u) -> vec<decltype(v.x+u.x)>{
+  v += u;
+  return v;
+}
 
-bool approx(const vec&, const vec&, double acc=1e-6, double eps=1e-6);
+template <typename T, typename S>
+auto operator*(vec<T> v, S s){
+  v *= s;
+  return v;
+}
+
+template <typename T, typename S>
+auto operator*(S s, const vec<T>& v){
+  return v*s;
+}
+
+template <typename T, typename S>
+auto operator/(vec<T> v, S s){
+  v/=s;
+  return v;
+}
+
+template <typename T>
+auto smart_conj(const T& val){
+  if constexpr (std::is_same_v<T, std::complex<float>> ||
+                std::is_same_v<T, std::complex<double>> ||
+                std::is_same_v<T, std::complex<long double>>) {
+    return std::conj(val);
+  }
+  else return val;
+}
+
+template <typename T, typename U>
+auto dot(const vec<T>& v, const vec<U>& u) -> decltype(v.x*u.x){
+    return smart_conj(v.x)*u.x + smart_conj(v.y)*u.y + smart_conj(v.z)*u.z;
+}
+
+template <typename T>
+auto norm(const vec<T>& v){
+  return std::sqrt(std::abs(dot(v, v)));
+}
+
+template <typename T, typename U>
+auto cross(const vec<T>& v, const vec<U>& u){
+  return vec(v.y*u.z-v.z*u.y, v.z*u.x-v.x*u.z, v.x*u.y-v.y*u.x);
+}
+
+template <typename T>
+std::vector<vec<T>> GramSchmidt(const std::vector<vec<T>>& input){
+  std::vector<vec<T>> basis;
+  for(const auto& v : input){
+    vec<T> w = v;
+    for(const auto u: basis){
+      auto proj_scale = dot(u, w)/dot(u,u);
+      w -= u*proj_scale;
+    }
+    auto w_norm = norm(w);
+    if(w_norm > 1e-9) basis.push_back(w/w_norm);
+  }
+  return basis;
+}
+
+template <typename T, typename U>
+bool approx(const vec<T>& v, const vec<U>& u, double acc=1e-6, double eps=1e-6){
+  if(std::abs(v.x - u.x) < acc 
+    && std::abs(v.y - u.y) < acc
+    && std::abs(v.z - u.z) < acc){
+    return true;
+  }
+  else if(std::abs(v.x-u.x)/std::max(std::abs(v.x), std::abs(u.x)) <= eps 
+      && std::abs(v.y-u.y)/std::max(std::abs(v.y), std::abs(u.y)) <= eps 
+      && std::abs(v.z-u.z)/std::max(std::abs(v.z), std::abs(u.z)) <= eps){
+    return true;
+  }
+  else return false;
+}
