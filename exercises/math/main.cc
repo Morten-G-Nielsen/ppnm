7b86e20046e08daea62cf7f86a2adf4f95969803
Morten-G-Nielsen <Mortengwn@live.dk>
2026-02-25 11:38:02 +0100

    added complex numbers

--- exercises/math/main.cc
+++ exercises/math/main.cc
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <cmath>
-#include <numbers>
-#include <cstdio>
+#include <complex>
 #include "sfuns.h"
+
+const double PI = 3.1415926535897932; 
+const std::complex<double> I(0,1);
 int main(){
-  std::cout << "sqrt(2)=" << std::sqrt(2.0) << "\n";
-  std::cout << "2^(1/5)=" << std::exp2(1.0/5) << "\n";
-  std::cout << "e^(pi)=" << std::exp(std::numbers::pi) << "\n";
-  std::cout << "pi^(e)=" << std::pow(std::numbers::pi, std::exp(1)) << "\n\n";
+  std::cout << "sqrt(2) = " << std::sqrt(2.0) << "\n";
+  std::cout << "2^(1/5) = " << std::exp2(1.0/5) << "\n";
+  std::cout << "e^(pi) = " << std::exp(PI) << "\n";
+  std::cout << "e^i = " <<  std::exp(I) << "\n";
+  std::cout << "pi^(e) = " << std::pow(PI, std::exp(1)) << "\n";
+  std::cout << "pi^i = " << std::pow(PI, I) << "\n";
+  std::cout << "i^i = " << std::pow(I, I) << "\n";
+  std::cout << "log(i) = " << std::log(I) << "\n\n";
 
   for(double x=1;x<=10;x+=1)
     std::cout << "fgamma("<< x <<")=" << sfuns::fgamma(x)
